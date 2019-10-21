da0fa172158e7b201f2346ad59beff82b2e3a519
G3TLeighton <gwl@g3t.uk.com>
2019-10-21 12:16:19 +0100

    add book routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/books', 'BooksController@store');
+Route::patch('/books/{book}', 'BooksController@update');
