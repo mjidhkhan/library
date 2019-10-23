258de688e468b70025bad8dd193f97b11dc0aa89
G3TLeighton <gwl@g3t.uk.com>
2019-10-23 15:46:33 +0100

    view resource added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Library') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,6 +21,7 @@
 </head>
 <body>
     <div id="app">
+        {{-- 
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -71,8 +72,8 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
+--}}
+        <main class="py-4 bg-gray-200 h-screen">
             @yield('content')
         </main>
     </div>
